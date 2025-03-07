ab5ebd95233ab16931ad22a15c9f9c54f84d154b
AS <ahmedselo2005@gmail.com>
2025-03-07 17:26:56 +0300

    custom user model and add the missing requirements.txt file

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="testuser",
+            email="testuser@example.com",
+            password="test1234",
+        )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        superuser = User.objects.create_superuser(
+            username="testsuperuser",
+            email="testsuperuser@example.com",
+            password="testpass1234",
+        )
+        self.assertEqual(superuser.username, "testsuperuser")
+        self.assertEqual(superuser.email, "testsuperuser@example.com")
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
